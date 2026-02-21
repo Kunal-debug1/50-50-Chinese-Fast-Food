caecdc231fd1f46c6f6473c9f83106bee7441040
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2026-02-21 16:16:15 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,17 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg
+from psycopg.rows import dict_row
 
 
 def get_connection():
-    database_url = os.getenv("postgresql://admin:i4t9FWZBUAVj0k8wKydiFqaXMwGpBoBT@dpg-d6co1s95pdvs73f7ppl0-a/hotel_db_slna")
+    database_url = os.getenv("DATABASE_URL")
 
     if not database_url:
         raise Exception("DATABASE_URL environment variable not set")
 
-    conn = psycopg2.connect(
+    conn = psycopg.connect(
         database_url,
-        cursor_factory=RealDictCursor
+        row_factory=dict_row
     )
 
     return conn
