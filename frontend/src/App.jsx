510431014c2d5b55a601910cd4104c2507ab5ce1
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2026-02-22 09:25:40 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
 import OrderStatus from "./pages/OrderStatus";
 import AdminDashboard from "./pages/AdminDashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
 import AdminLogin from "./pages/AdminLogin";
+import ProtectedRoute from "./components/ProtectedRoute";
 import AdminProtectedRoute from "./components/AdminProtectedRoute";
 
 function App() {
@@ -15,64 +15,17 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-
         <Route path="/" element={<Home />} />
         <Route path="/tables" element={<TableSelection />} />
 
-        <Route
-          path="/menu"
-          element={
-            <ProtectedRoute>
-              <Menu />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/cart"
-          element={
-            <ProtectedRoute>
-              <Cart />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/status"
-          element={
-            <ProtectedRoute>
-              <OrderStatus />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/menu" element={<ProtectedRoute><Menu /></ProtectedRoute>} />
+        <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+        <Route path="/status" element={<ProtectedRoute><OrderStatus /></ProtectedRoute>} />
 
         <Route path="/admin-login" element={<AdminLogin />} />
-
-        <Route
-          path="/admin"
-          element={
-            <AdminProtectedRoute>
-              <AdminDashboard />
-            </AdminProtectedRoute>
-          }
-        />
-        
-        <Route path="/admin-login" element={<AdminLogin />} />
-
-        <Route
-          path="/admin"
-          element={
-            <AdminProtectedRoute>
-              <AdminDashboard />
-            </AdminProtectedRoute>
-          }
-        />
-
-
+        <Route path="/admin" element={<AdminProtectedRoute><AdminDashboard /></AdminProtectedRoute>} />
       </Routes>
     </BrowserRouter>
-
-    
   );
 }
 
